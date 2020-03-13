b555ca4078f05f1978b3a968a0472ca02d64da9d
高欧凡（Gao Oufan） <2726788990@qq.com>
2020-03-13 08:49:07 +0800

    Changes v5.0001
    Added functionality, changes described in https://www.efsdk.com/hlp4

--- acx/Color.js
+++ acx/Color.js
@@ -1,45 +1,37 @@
-EF.Color=function(v,h) {
-	var _=EF,o=this,t=_.xT(v); if(!h) h=0;
-	if(t=="N") v=(t=="N"?[v>>16,(v>>8)&0xff,v&0xff]:_.aA(v));
-	while(v.length<4) v.push(255);
-	
-	if(h<2) o.V=v; else {
-		var d=[0,0,0,0,0],i; for(i in d) d[i]=b64.indexOf(v[i]);
-		o.V=[d[0]*4+(d[1]>>4),(d[1]&15)*16+(d[2]>>2),(d[2]&3)*64+d[3],d[4]*4];
-	}
-	if(h==1) {
-		if(t=="N") o.V[0]*=6/256;
-		var c=o.V,G=["FXO","RFO","OFX","ORF","XOF","FOR"],M=Math.floor(c[0]),H=[],S,L,i;
-		if(M>=6) M-=6; L=Math.floor((c[0]-M)*255); S={ F:255,O:0,X:L,R:255-L };
-		for(i=0 ; i<3 ; i++) {
-			H.push(S[G[M][i]]); H[i]+=Math.floor(((128-H[i])*(255-c[1]))/255);
-			if(c[2]>128) H[i]+=Math.floor(((255-H[i])*(c[2]-128))/127);
-			else H[i]-=Math.floor((H[i]*(128-c[2]))/128);
-		}
-		o.V=H; if(c.length>3) o.V.push(c[3]);
-	}
-	o.toString=function(y) { return y?o.V.join(','):o.DOM().outerHTML; }
-	o.HSL=function() {
-		var c=o.V,G,M,H=[],i; for(i=0 ; i<3 ; i++) c[i]=Math.floor(c[i]);
-		G=Math.max(c[0],c[1],c[2]); M=Math.min(c[0],c[1],c[2]); H=[0,G-M,Math.floor((G+M)/2)];
-		if(H[1]>0) H[0]=(G==c[0])?((c[1]-c[2])/H[1]+((c[1]<c[2])?6:0)):((G==c[1])?((c[2]-c[0])/H[1]+2):((c[0]-c[1])/H[1]+4));
-		return H;
-	}
-	o.REV=function(x) { var v=o.V; return (v[0]+v[1]+v[2])<384?(x?255:'#fff'):(x?0:'#000'); }
-	o.CSS=function() {
-		var v=o.V; if(v[3]==255) {
-			var w='#',a="0123456789abcdef",i,k=(v[0]%17||v[1]%17||v[2]%17)?1:0;
-			for(i=0 ; i<3 ; i++) w+=a[Math.floor(v[i]/16)]+(k?a[v[i]%16]:""); return w;
-		}
-		else return 'rgba('+[v[0],v[1],v[2],(v[3]/255).toFixed(3)].join(',')+')';
-	}
-	o.DOM=function(w) {
-		var f=_.xM('div',0,0,[w||"10em"],{ border:"1px solid #000",background:o.CSS(),color:o.REV(),textAlign:"center" });
-		f.innerText=o.toString(1); return f;
-	}
-	o.Int=function() { return o.V?(o.V[0]<<16)+(o.V[1]<<8)+o.V[2]:0; }
-	o.cpy=function() { return new _.Color(o.V); }
-	o.EFON=function() { var c=o.V; return b64[c[0]>>2]+b64[(c[0]&3)*16+(c[1]>>4)]+b64[(c[1]&15)*4+(c[2]>>6)]+b64[c[2]&63]+b64[c[3]>>2]; }
-	o.EQ=function(c) { for(var i=0 ; i<4 ; i++) if(c.V[i]!==o.V[i]) return; return 1; }
+(function(){ var _=EF; _.Use(["acx/DH","acx/Math"]);
+_.Color=function() { var o=this,z=arguments; 
+o.set=function(r,g,b,a) { o.r=r; o.g=g; o.b=b; o.a=_.xT(a)?a:1; return o; }
+o.setN=function(n) {
+	var k=Math.floor; o.r=k(n/16777216); o.g=k((n/65536)&255);
+	o.b=k((n/256)&255); o.a=k(n&255); return o;
 }
-EF.RQ--;
+o.setHSL=function(h,s,l,a){
+	h*=6; var G=["FXO","RFO","OFX","ORF","XOF","FOR"],M=Math.floor(h),S,L,i;
+	if(M>=6) M-=6; L=h-M; S={F:1,O:0,X:L,R:1-L};
+	return o.set(iA(S[G[M][0]]),iA(S[G[M][1]]),iA(S[G[M][2]]),a);
+	function iA(v) { return v+(0.5-v)*(1-s)+(l>0.5?(((1-v)*(l-0.5))*2):-((v*(0.5-l))*2)); }
+}
+o.toString=function(y) { return y?o.Hex():o.DOM().outerHTML; }
+o.HSL=function() {
+	var G=Math.max(o.r,o.g,o.b),M=Math.min(o.r,o.g,o.b),r={}; r.s=G-M; r.l=(G+M)/2;
+	if(r.s) r.h=((G==o.r)?((o.g-o.b)/r.s+((o.g<o.b)?6:0)):((G==o.g)?((o.b-o.r)/r.s+2):((o.r-o.g)/r.s+4)))/6;
+	return r;
+}
+o.inv=function(x){ return (o.r+o.g+o.b)<1.5?(x?1:'#fff'):(x?0:'#000'); }
+o.Int=function() {
+	return (iA(o.r)*16777216)+(iA(o.g)*65536)+(iA(o.b)*256)+iA(_.xT(o.a)?o.a:1);
+	function iA(v) { return Math.floor(v*255); }
+}
+o.CSS=function() { return '#'+_.a0(o.Int(),8,16); }
+o.DOM=function(w) {
+	var f=_.xM('div',0,0,[w||"10em"],{border:"1px solid #000",background:o.CSS(),color:o.Inv(),textAlign:"center"});
+	f.innerText=o.Hex(); return f;
+}
+o.cpy=function(c) { return o.set(c.r,c.g,c.b,c.a); }
+o.clone=function() { return new _.Color(o); }
+o.eq=function(c) { var k="rgba"; for(var i=0 ; i<4 ; i++) if(c[k[i]]!==o[k[i]]) return; return 1; }
+o.scale=function(s,a) { return new _.Color(o.r*s,o.g*s,o.b*s,o.a*(a?s:1)); }
+if(z.length==1) return _.xT(z[0])=="N"?o.setN(z[0]):o.cpy(z[0]);
+if(z.length>=3) return o.set(z[0],z[1],z[2],_.xT(z[3])?z[3]:1);
+return o; }
+_.RQ--; })();
